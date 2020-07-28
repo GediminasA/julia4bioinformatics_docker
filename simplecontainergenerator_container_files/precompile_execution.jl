7be72b6ad0e3f7cdd4074c658cf03804c28f4399
Gediminas Alzbutas <gediminas.alzbutas@thermofisher.com>
2020-07-28 09:14:22 +0300

    Added more libs

--- simplecontainergenerator_container_files/precompile_execution.jl
+++ simplecontainergenerator_container_files/precompile_execution.jl
@@ -1,13 +1,20 @@
+import ArgParse # pkg_names_to_import
 import CSV # pkg_names_to_import
+import Clustering # pkg_names_to_import
 import CodecZlib # pkg_names_to_import
 import DataFramesMeta # pkg_names_to_import
 import DataFrames # pkg_names_to_import
-import ArgParse # pkg_names_to_import
 import DataStructures # pkg_names_to_import
+import FASTX # pkg_names_to_import
+import FastaIO # pkg_names_to_import
+import IntervalSets # pkg_names_to_import
 import OrderedCollections # pkg_names_to_import
+import Query # pkg_names_to_import
+import Statistics # pkg_names_to_import
+import StringDistances # pkg_names_to_import
+import StatsBase # pkg_names_to_import
 import TextWrap # pkg_names_to_import
 import XAM # pkg_names_to_import
-import FASTX # pkg_names_to_import
 
 import Pkg
 for (uuid, info) in Pkg.dependencies()
